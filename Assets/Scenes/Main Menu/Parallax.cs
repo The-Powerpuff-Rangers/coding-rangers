a295d35fc0144d5a4872199e8fd5e32df3df4bbd
2002Bishwajeet <bishwajeet.techmaster@gmail.com>
2022-10-01 16:57:14 +0530

    parallax done

--- Assets/Scenes/Main Menu/Parallax.cs
+++ Assets/Scenes/Main Menu/Parallax.cs
@@ -14,21 +14,35 @@ public class Parallax : MonoBehaviour
     /// <summary>
     /// The parallax effect is based on the difference between the camera's current position and the position in the previous frame.
     /// </summary>
-    [SerializeField] private float parrallaxEffectMultiplier = 0;
+    [SerializeField] private Vector2 parrallaxEffectMultiplier;
+
+    private float textureUnitSizeX;
 
     // Start is called before the first frame update
     private void Start()
     {
         cameraTransform = Camera.main.transform;
         lastcameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+
 
     }
 
     // Update is called once per frame
     private void LateUpdate()
     {
+        //Update the parallax effect automatically
+
         Vector3 deltaMovement = cameraTransform.position - lastcameraPosition;
-        transform.position += deltaMovement * parrallaxEffectMultiplier;
+        transform.position += new Vector3(deltaMovement.x * parrallaxEffectMultiplier.x, deltaMovement.y * parrallaxEffectMultiplier.y);
         lastcameraPosition = cameraTransform.position;
+
+        if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+        {
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y, transform.position.z);
+        }
     }
 }
