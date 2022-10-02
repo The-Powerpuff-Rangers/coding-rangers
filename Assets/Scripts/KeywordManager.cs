8d3740a193f24a2c1a31f919da51408b20373604
Kranium2002 <vidhumathur2002@gmail.com>
2022-10-02 07:43:20 +0530

    hangman

--- Assets/Scripts/KeywordManager.cs
+++ Assets/Scripts/KeywordManager.cs
@@ -8,8 +8,8 @@ public class KeywordManager : MonoBehaviour
 {
     // Create a Dictionary. The key value would be the string and the value would be the int
     // 0- stands for the value hasn't been set yet
-    // 1- stands for the value has been set
-    public Dictionary<string, int> keywords = new Dictionary<string, int>();
+    // 1- stands for the value has been 
+
 
     [SerializeField] private int level;
 
@@ -18,37 +18,30 @@ public class KeywordManager : MonoBehaviour
  public   SpriteRenderer spriteRenderer;
  public Sprite sprite1;
 
-
+    private string[] arr = { "F", "O", "R", "M" };
     // Start is called before the first frame update
     void Start()
     {
         var something = gameObject.GetComponent<TextMesh>();
         // Add the keywords to the dictionary
-        if (level == 1)
-        {
-            keywords.Add("f", 0);
-            keywords.Add("o", 0);
-            keywords.Add("r", 0);
-            keywords.Add("m", 0);
-        }
-        else if (level == 2)
-        {
-            keywords.Add("i", 0);
-            keywords.Add("n", 0);
-            keywords.Add("p", 0);
-            keywords.Add("u", 0);
-            keywords.Add("t", 0);
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(keywords["f"] == 1)
-        {
 
+    }
 
-        }
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        string currStr = collision.gameObject.tag;
+       // for(int i=0;i<4;i++)
+        //{
+          //  if(currStr != arr[i])
+           // {
+                
+           // }
+       // }
     }
 }
